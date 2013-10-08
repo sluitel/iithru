f04cab74c440484049a9ab7a0c88801cf2054d80
Subash Luitel <Subash@Subashs-MacBook-Pro.local>
2013-10-08 16:59:56 -0500

    fixed pic resizing

---
+++ view:controller/IIThru/IIThru/MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  IIThru
+//
+//  Created by Subash Luitel on 10/8/13.
+//  Copyright (c) 2013 IIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+
+@end
