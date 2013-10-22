2a44c2d7955eb4b28edfc9b53b17faeb87bd4bf6
William Bafia <wbafia@hawk.iit.edu>
2013-10-22 14:07:09 -0500

    Added maps code

---
+++ Mapstest/Mapstest/IPROAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPROAppDelegate.h
+//  Mapstest
+//
+//  Created by William Bafia on 10/7/13.
+//  Copyright (c) 2013 Illinois Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPROAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mapstest/Mapstest/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  Mapstest
+//
+//  Created by William Bafia on 10/7/13.
+//  Copyright (c) 2013 Illinois Institute of Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+#import "IPROAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IPROAppDelegate class]));
+    }
+}
